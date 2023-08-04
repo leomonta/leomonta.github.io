751d82bd7a4def3007ad11f302f2dedfd9e26a41
Leonard <remon.w.dev@gmail.com>
2023-08-04 18:05:54 +0200

    what even is web development

--- scripts/compile_tailwindcss.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source colors
-
-printf "${BBlue}npx${Cyan} tailwindcss -m -i ./src/style/tw.css -o ./build/style/tw.css${Reset}\n"
-npx tailwindcss -m -i ./src/style/tw.css -o ./build/style/tw.css
